d72a121077b5881455ada30f6b78f9f4fa627904
Juris <juris.abolins.cs@gmail.com>
2022-02-12 14:44:40 +0200

    Final version

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,18 +6,20 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client implements Runnable {
-    private Socket socket;
-    private String name;
+    /**
+     * name- String value (not unique) to identify the user and display to other users
+     */
+    private String nameOfUser;
+    private final Socket socket;
+    private final Server server;
     private InputStream inputStream;
     private OutputStream outputStream;
     private Scanner in;
     private PrintStream out;
-    private Server server;
 
     public Client(Socket socket, Server server) {
         this.socket = socket;
         this.server = server;
-       //palaižam plūsmu
         new Thread(this).start();
     }
 
@@ -29,22 +31,20 @@ public class Client implements Runnable {
             in = new Scanner(inputStream);
             out = new PrintStream(outputStream);
 
-            // lasām no tīkla un rakstām tīklā
             out.println("Welcome to chat");
 
             out.println("Please enter your name or \"bye\"");
             String inputName = in.nextLine();
             if (inputName.equals("bye")) socket.close();
-            name = inputName;
-            server.sendAll( name+ " is connected to our chat");
+            nameOfUser = inputName;
+            server.sendMessageToAll( nameOfUser + " is connected to our chat");
 
-            String input = in.nextLine();
-            while (!input.equals("bye")) {
-                server.sendAll(name + ": " + input);
-                input = in.nextLine();
+            String inputMessage = in.nextLine();
+            while (!inputMessage.equals("bye")) {
+                server.sendMessageToAll(nameOfUser + ": " + inputMessage);
+                inputMessage = in.nextLine();
             }
-            server.sendAll(name + " left chat");
-            System.out.println(name + " left chat");
+            server.sendMessageToAll(nameOfUser + " left chat");
             socket.close();
 
         } catch (IOException e) {
@@ -52,7 +52,7 @@ public class Client implements Runnable {
         }
     }
 
-    void receive(String message) {
+    public void receive(String message) {
         out.println(message);
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
+    /**
+     * list of all connections (chat server users)
+     */
     List<Client> clientList = new ArrayList();
     ServerSocket serverSocket;
 
@@ -14,25 +17,24 @@ public class Server {
 
     public void run() {
         while (true) {
-            System.out.println("Waiting....");
             try {
                 Socket socket = serverSocket.accept();
-
                 Client client = new Client(socket, this);
-                System.out.println("New client is connected");
                 clientList.add(client);
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    void sendAll(String message) {
+    /**
+     * @param message- String value that will be received from the Client
+     *                 (usually entered by the user) and sent to all chat users
+     */
+    public void sendMessageToAll(String message) {
         for (Client client : clientList) {
             client.receive(message);
         }
     }
-
 
 }
